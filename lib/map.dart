314ae64a406287d69d109cf34923a1766adbf355
Fady Elhussein <83413691+Fady-Elhussein@users.noreply.github.com>
2024-10-31 11:49:44 +0300

    Initial commit

---
+++ lib/map.dart
@@ -0,0 +1,34 @@
+void main(){
+var fady=[
+  {"fady":101},
+  {"fady":102},
+  {"fady":103},
+  {"fady":104},
+];
+
+var x =fady.map((x){
+  print(x);
+  return
+    {
+      "dsghcfghdfhg":x["fady"]
+    };
+
+
+}).toList();
+print(x);
+
+
+// var x=fady.map(
+//         (e){
+//
+//
+//   return {
+//     "name":e["fady"],
+//
+//
+//   };
+// });
+// print(x);
+// print(x.runtimeType);
+
+}
